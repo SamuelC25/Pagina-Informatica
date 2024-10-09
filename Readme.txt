8c0a5b616114d1b0666b9877b4f8ba4b781a3791
Samuel Correa <sichero20@gmail.com>
2024-10-09 20:43:57 -0300

    Proyecto BootsTrap informativa

---
+++ Readme.txt
@@ -0,0 +1,9 @@
+----------- Cosas por mejorar de la pagina -------------
+
+1) No es responsive, tratar de que sea responsive para todo tipo.
+
+2) Mejorar cada una de las secciones que en cada una encontraras un error.
+
+3) Vas a necesitar ayuda para armar o mejorar solo.
+
+4) 
---
+++ componentes.js
@@ -0,0 +1,113 @@
+/* MOVIMIENTO DE CARRUSEL
+const myCarousel = document.querySelector('#carouselExampleCaptions');
+  const carousel = new bootstrap.Carousel(myCarousel, {
+    interval: 1000, // 1000 ms = 1 segundos
+    ride: 'carousel'
+  });
+*/
+/* MOVIMIENTO HEADER JS-QUERY */
+document.addEventListener('DOMContentLoaded', function() {
+  particlesJS("particles-js", {
+      "particles": {
+          "number": {
+              "value": 80,
+              "density": {
+                  "enable": true,
+                  "value_area": 800
+              }
+          },
+          "color": {
+              "value": "#ffffff"
+          },
+          "shape": {
+              "type": "circle",
+              "stroke": {
+                  "width": 0,
+                  "color": "#000000"
+              },
+              "polygon": {
+                  "nb_sides": 5
+              }
+          },
+          "opacity": {
+              "value": 0.5,
+              "random": false,
+              "anim": {
+                  "enable": false,
+                  "speed": 1,
+                  "opacity_min": 0.1,
+                  "sync": false
+              }
+          },
+          "size": {
+              "value": 20,
+              "random": true,
+              "anim": {
+                  "enable": false,
+                  "speed": 40,
+                  "size_min": 0.1,
+                  "sync": false
+              }
+          },
+          "line_linked": {
+              "enable": true,
+              "distance": 150,
+              "color": "#ffffff",
+              "opacity": 0.22,
+              "width": 1
+          },
+          "move": {
+              "enable": true,
+              "speed": 6,
+              "direction": "none",
+              "random": false,
+              "straight": false,
+              "out_mode": "out",
+              "attract": {
+                  "enable": false,
+                  "rotateX": 600,
+                  "rotateY": 1200
+              }
+          }
+      },
+      "interactivity": {
+          "detect_on": "canvas",
+          "events": {
+              "onhover": {
+                  "enable": true,
+                  "mode": "repulse"
+              },
+              "onclick": {
+                  "enable": true,
+                  "mode": "push"
+              },
+              "resize": true
+          },
+          "modes": {
+              "grab": {
+                  "distance": 400,
+                  "line_linked": {
+                      "opacity": 1
+                  }
+              },
+              "bubble": {
+                  "distance": 400,
+                  "size": 40,
+                  "duration": 2,
+                  "opacity": 8,
+                  "speed": 3
+              },
+              "repulse": {
+                  "distance": 200
+              },
+              "push": {
+                  "particles_nb": 4
+              },
+              "remove": {
+                  "particles_nb": 2
+              }
+          }
+      },
+      "retina_detect": true
+  });
+});
